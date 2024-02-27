c54be53fe9691e369ed25a9a0b10a1143bc09ac6
Thiago Macedo <81458976+thiagoMacedoPRJ@users.noreply.github.com>
2024-02-26 22:25:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ import os
 # ////////////////////////////////////////////// STORAGE GOSTEIS ///////////////////////////////////////////////
 
 
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+#scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
-creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+#creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 
-client = gspread.authorize(creds)
+#client = gspread.authorize(creds)
 
-sheet = client.open("Contador-Escola").sheet1  # Prorcure na pasta aonde você tem permissão o nome XXXX e depois..
+#sheet = client.open("Contador-Escola").sheet1  # Prorcure na pasta aonde você tem permissão o nome XXXX e depois..
 
-data = sheet.get_all_records()  # Obtenha todos as informaçoes desta arquivo.
+#data = sheet.get_all_records()  # Obtenha todos as informaçoes desta arquivo.
 
-coluna = sheet.col_values(1)[1:] # pegue os gosteis
+#coluna = sheet.col_values(1)[1:] # pegue os gosteis
     
     
 
@@ -70,15 +70,15 @@ app.secret_key = 'biosec'
 def index():
     
     if request.method == "GET":
-        gostei = sheet.col_values(1)[1:] # pegue os gosteis
-        gostei2 = sheet.col_values(2)[1:] # pegue os gosteis2
-        gostei3 = sheet.col_values(3)[1:] # pegue os gosteis3
+        gostei = 10 # pegue os gosteis
+        gostei2 = 10 # pegue os gosteis2
+        gostei3 = 10 # pegue os gosteis3
 
-        return render_template('index.html', gosteis=gostei[0], gosteis2=gostei2[0], gosteis3=gostei3[0])
+        return render_template('index.html', gosteis=gostei, gosteis2=gostei2, gosteis3=gostei3)
     else:
         data = list(request.form.to_dict())[0]
         print(data)
-        serveUP(data)
+        #serveUP(data)
         return "nada."
 
 
@@ -156,3 +156,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
